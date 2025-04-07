4a17bd0980eacbff74a2f563e3e79791a31f0b0f
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-04-07 15:44:32 +0200

    7-fixa del-4 Adress_information

---
+++ app/components/footer/footer-bar/footer-adress-info.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function FooterAdressInfo() {
+  return (
+  <div className="">
+    <h3 className="">ADDRESS</h3>
+        <ul className="">
+            <li>📍 Projektgrändgatan 11,<br /> 543 21 Gasumawi</li>
+        </ul>
+ </div>
+  );
+}
