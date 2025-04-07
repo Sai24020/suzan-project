a8180c2af604b93cdc463a0c820325cd8bef3e9b
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-04-07 15:43:51 +0200

    6-fixa del-3 Kontaktinformation

---
+++ app/components/footer/footer-bar/footer-kontakt-info.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function FooterKontaktInfo() {
+  return (
+    <div className="">
+      <h3 className="">CONTACT</h3>
+      <ul className="">
+        <li className="">📞 +46 761 981 11</li>
+        <li className="">✉️ sm_pal_2025@info.se</li>
+     </ul>
+    </div>
+  );
+}
