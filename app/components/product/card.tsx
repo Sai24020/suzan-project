b403eb37780d1c2ec8f13fc86105344f338e3453
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-04-10 15:16:32 +0200

    2_UI-card design ny

--- app/components/product/card.tsx
+++ app/components/product/card.tsx
@@ -4,17 +4,18 @@ import { Product } from "@/lib/interfaces";
 import Link from "next/link";
 import Image from "next/image";
 import styles from './card.module.css'; // Import the CSS module
+import { FaCartPlus, FaHeart, FaMinusCircle, FaPlusCircle } from "react-icons/fa";
+import { Button } from "../ui/button";
 
 // Enskilt produktkort
 export function Card({ product }: { product: Product }) {
 
   return (
-    <li className={styles.cardContainer}>
-            <div className={styles.cardTitelContainer}>
-            <h2 className={styles.cardTitle}>{product.title}</h2>
-            </div>
-      <div className={styles.cardImageContainer}>
-        <Link href={`/products/${product.id}`}>
+    <li className={styles.cardContainer}>    
+      <div className={styles.cardImageContainer}> 
+      <h2 className={styles.cardTitle}>{product.title}</h2>
+         <span className={styles.cardDiscount}>{product.discountPercentage}%</span>
+        <Link href={`/products/${product.id}`}>              
           <Image
             className={styles.cardImage}
             src={product.thumbnail}
@@ -23,13 +24,30 @@ export function Card({ product }: { product: Product }) {
             alt={`Bild ${product.title}`}
           />
         </Link>
- 
+        <ul className={styles.cardBrand}><li className="ml-1 mr-1">{product.brand}</li></ul>
+
+        <p className={styles.cardPrice}>${product.price}</p>
+        <ul className="flex ">
+        <span className="text-white"> . </span>
+      <li className="rounded bg-blue-700 text-amber-300 hover:bg-blue-400 text-center">
+      <Button className="ml-2">
+            <FaPlusCircle /> 
+      </Button>
+      </li>
+      <span>1</span>
+      <li className="rounded bg-blue-700 text-amber-300 hover:bg-blue-400 text-center">
+      <Button className="ml-2">
+            <FaMinusCircle /> 
+      </Button>
+      </li>
+      <span className="text-white"> . </span>
+      <li className="rounded bg-blue-700 text-amber-300 hover:bg-blue-400 text-center">
+      <Button className="">
+            ADD TO CART <FaCartPlus /> 
+      </Button>
+      </li>
+      </ul>
       </div>
-        <div className={styles.cardPriceContainer}>
-          <p className={styles.cardPrice}>${product.price}</p>
-          <span className={styles.cardDiscount}>{product.discountPercentage}%</span>
-        </div>
-  
     </li>
   );
 }
